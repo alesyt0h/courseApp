957e776376862dc2cfef4704896547b0aed0e059
Alex Ortigosa <alexortigosa179@gmail.com>
2022-08-13 13:35:19 +0200

    Basic routing with child routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { ChapterCardComponent } from './components/chapter-card/chapter-card.component';
+import { ResumeCourseComponent } from './components/resume-course/resume-course.component';
+import { SessionCardComponent } from './components/session-card/session-card.component';
+import { HomeComponent } from './home/home.component';
+import { ChaptersModule } from './chapters/chapters.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ChapterCardComponent,
+    ResumeCourseComponent,
+    SessionCardComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ChaptersModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/chapters/chapters-routing.module.ts
+++ src/app/chapters/chapters-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SessionPageComponent } from './pages/session-page/session-page.component';
+import { ChapterPageComponent } from './pages/chapter-page/chapter-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ChapterPageComponent },
+  { path: 'session/:id', component: SessionPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
